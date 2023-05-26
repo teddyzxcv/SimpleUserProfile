089226dcd68921269f8a4b1c5aa0cdba83f58fcb
teddyzxcv <48234309+teddyzxcv@users.noreply.github.com>
2023-05-26 18:27:44 +0300

    Initial Commit

---
+++ UserProfile/UserProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserProfileApp.swift
+//  UserProfile
+//
+//  Created by ZhengWu Pan on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct UserProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
